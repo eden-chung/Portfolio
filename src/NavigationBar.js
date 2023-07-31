21d7e6e61244a157e4b1eeff673d210d365f559c
eden-chung <edenchung23@gmail.com>
2023-07-31 19:08:52 +0200

    init

---
+++ src/NavigationBar.js
@@ -0,0 +1,133 @@
+import {
+    Flex,
+    Button,
+    Drawer,
+    DrawerBody,
+    DrawerOverlay,
+    DrawerContent,
+    Stack,
+    IconButton,
+    useDisclosure,
+    HStack,
+    Link,
+  } from "@chakra-ui/react";
+  import { HamburgerIcon } from "@chakra-ui/icons";
+  import { useState } from "react";
+  
+  export default function Nav() {
+    const colorwhite = '#FFFFFF';
+
+    const [scroll, setScroll] = useState(false);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+  
+    const scrollToStart = () => {
+      const heroStart = document.querySelector("#hero");
+      heroStart.scrollIntoView({ behavior: "smooth" });
+    };
+    const scrollToAbout = () => {
+      const aboutSection = document.querySelector("#about");
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+    };
+    const scrollToProjects = () => {
+      const projectsSection = document.querySelector("#projects");
+      projectsSection.scrollIntoView({ behavior: "smooth" });
+    };
+    const scrollToContact = () => {
+      const contactSection = document.querySelector("#contact");
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    };
+    const openResume = () => {
+      const resumePath = '/assets/resume.pdf';
+  
+      window.open(resumePath, '_blank');
+    };
+    const changeScroll = () =>
+      document.body.scrollTop > 80 || document.documentElement.scrollTop > 80
+        ? setScroll(true)
+        : setScroll(false);
+  
+    window.addEventListener("scroll", changeScroll);
+  
+    const TbLetterComponents = [];
+  
+  
+    return (
+      <>
+        <Flex
+          bg={"gray.900"}
+          px={4}
+          h={16}
+          boxShadow={scroll ? "base" : "none"}
+          zIndex="sticky"
+          position="fixed"
+          as="header"
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          w="100%"
+        >
+          <Link onClick={scrollToStart}>
+            <HStack>
+              {TbLetterComponents.map((Component, index) => (
+                <Component key={index}/>
+              ))}
+            </HStack>
+          </Link>
+  
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              {true ? (
+                <>
+                  <Button variant="ghost" onClick={scrollToAbout} style={{color:"white"}}>
+                    About
+                  </Button>
+                  <Button variant="ghost" onClick={scrollToProjects} style={{color:"white"}}>
+                    Projects
+                  </Button>
+                  <Button variant="ghost" onClick={openResume} style={{color:"white"}}>
+                    Resume
+                  </Button>
+                  <Button variant="ghost" onClick={scrollToContact} style={{color:"white"}}>
+                    Contact
+                  </Button>
+                </>
+              ) : (
+                <></>
+              )}
+  
+              {true ? (
+                <></>
+              ) : (
+                <>
+                  <Button
+                    as={IconButton}
+                    icon={<HamburgerIcon />}
+                    onClick={onOpen}
+                  ></Button>
+                  <Drawer placement="top" onClose={onClose} isOpen={isOpen}>
+                    <DrawerOverlay />
+                    <DrawerContent>
+                      <DrawerBody>
+                        <Button variant="ghost" onClick={scrollToAbout} style={{color:colorwhite}}>
+                          About
+                        </Button>
+                        <Button variant="ghost" onClick={scrollToProjects} style={{color:colorwhite}}>
+                          Projects
+                        </Button>
+                        <Button variant="ghost" onClick={openResume} style={{color:colorwhite}}>
+                          Resume
+                        </Button>
+                        <Button variant="ghost" onClick={scrollToContact} style={{color:colorwhite}}>
+                          Contact
+                        </Button>
+                      </DrawerBody>
+                    </DrawerContent>
+                  </Drawer>
+                </>
+              )}
+            </Stack>
+          </Flex>
+        </Flex>
+      </>
+    );
+  }
+  
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+      colors: {
+        primary: '#3182CE',
+        secondary: '#805AD5',
+        // Add more custom colors here
+    },
+});
+
+export default theme;
