4de52c9b396b533541959bc0bda9f8a44c52f5ae
eden-chung <edenchung23@gmail.com>
2023-08-01 01:07:26 +0200

    modify browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import NavigationBar from './NavigationBar';
 import Home from './Home';
 import Projects from './Projects';
 import Contact from './Contact';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
 function App() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Box bg={"gray.800"}>
                 <NavigationBar/>
                 <Home/>
                 <Projects/>
                 <Contact/>
             </Box>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
