ec9209339c2f4a95ac00a454ea282bd815811f0c
eden-chung <edenchung23@gmail.com>
2023-08-01 00:33:30 +0200

    modify package.json

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ import NavigationBar from './NavigationBar';
 import Home from './Home';
 import Projects from './Projects';
 import Contact from './Contact';
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
   return (
-    <Box bg={"gray.800"}>
-      <NavigationBar/>
-      <Home/>
-      <Projects/>
-      <Contact/>
-    </Box>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Box bg={"gray.800"}>
+        <NavigationBar/>
+        <Home/>
+        <Projects/>
+        <Contact/>
+      </Box>
+    </BrowserRouter>
   );
 }
 
