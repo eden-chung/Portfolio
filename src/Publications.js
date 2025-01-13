cb51085e58537aab33b44f3765cfe90a99eceaad
eden-chung <edenchung23@gmail.com>
2025-01-13 22:39:07 +0100

    fix rag paper

--- src/Publications.js
+++ src/Publications.js
@@ -68,7 +68,7 @@ export default function Publications() {
           <Stack px={4} spacing={5}>
             <VStack>
               <Box mb={20}>
-                <CustomLink href="https://arxiv.org/abs/2401.09555v1" isExternal>
+                <CustomLink href="https://arxiv.org/abs/2404.07221" isExternal>
                   <div
                     style={{
                       transition: "transform 0.3s ease-in-out",
@@ -89,7 +89,7 @@ export default function Publications() {
                       <Stack>
                         <CardBody align="left">
                           <Heading size="md">Improving Retrieval for RAG based Question Answering Models on Financial Documents</Heading>
-                          <Text py={2}>The effectiveness of Large Language Models (LLMs) in generating accurate responses relies heavily on the quality of input provided, particularly when employing Retrieval Augmented Generation (RAG) techniques. RAG enhances LLMs by sourcing the most relevant text chunk(s) to base queries upon. Despite the significant advancements in LLMs' response quality in recent years, users may still encounter inaccuracies or irrelevant answers; these issues often stem from suboptimal text chunk retrieval by RAG rather than the inherent capabilities of LLMs. To augment the efficacy of LLMs, it is crucial to refine the RAG process. This paper explores the existing constraints of RAG pipelines and introduces methodologies for enhancing text retrieval. It delves into strategies such as sophisticated chunking techniques, query expansion, the incorporation of metadata annotations, the application of re-ranking algorithms, and the fine-tuning of embedding algorithms. Implementing these approaches can substantially improve the retrieval quality, thereby elevating the overall performance and reliability of LLMs in processing and responding to queries. </Text>
+                          <Text py={2}>     The effectiveness of Large Language Models (LLMs) in generating accurate responses relies heavily on the quality of input provided, particularly when employing Retrieval Augmented Generation (RAG) techniques. RAG enhances LLMs by sourcing the most relevant text chunk(s) to base queries upon. Despite the significant advancements in LLMs' response quality in recent years, users may still encounter inaccuracies or irrelevant answers; these issues often stem from suboptimal text chunk retrieval by RAG rather than the inherent capabilities of LLMs. To augment the efficacy of LLMs, it is crucial to refine the RAG process. This paper explores the existing constraints of RAG pipelines and introduces methodologies for enhancing text retrieval. It delves into strategies such as sophisticated chunking techniques, query expansion, the incorporation of metadata annotations, the application of re-ranking algorithms, and the fine-tuning of embedding algorithms. Implementing these approaches can substantially improve the retrieval quality, thereby elevating the overall performance and reliability of LLMs in processing and responding to queries. </Text>
                         </CardBody>
                       </Stack>
                     </CardWithHoverEffect>
@@ -125,7 +125,7 @@ export default function Publications() {
                   </div>
                 </CustomLink>
               </Box>
-              <Box mb={-2}>
+              <Box mb={-2} mt={20}>
                 <CustomLink href="https://arxiv.org/abs/2404.07221v1" isExternal>
                   <div
                     style={{
